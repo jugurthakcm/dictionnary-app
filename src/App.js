3a9ddf6bcfb117b07914ae54aeca2d8acfad89a3
Jugurtha <jugurthakacimi11@gmail.com>
2021-11-08 08:06:30 -0600

    Word page coded

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import SearchBox from "./components/SearchBox";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import WordPage from "./components/WordPage";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<SearchBox />} />
+          <Route exact path="/" element={<SearchBox />} />
+          <Route path="/:word" element={<WordPage />} />
         </Routes>
       </div>
     </BrowserRouter>
